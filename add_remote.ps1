c65601964937214fdaeb4ccb42b969e77658f97a
Yogesh <yogesh@example.com>
2026-04-30 21:36:44 +0530

    Update Gift Finder with Amazon affiliate links

--- add_remote.ps1
+++
@@ -1,3 +0,0 @@
-$env:GIT_ASKPASS = "echo"
-$token = "ghp_S5k616jxVoK1MItobm8fblo2Hc0fDc1BSB09"
-& "C:\Program Files\Git\bin\git.exe" remote add origin https://$token@github.com/drawgifts/drawgifts.git
--- push.ps1
+++
@@ -1,6 +0,0 @@
-$env:GIT_ASKPASS = "echo"
-$env:GHP_TOKEN = "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"
-[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
-& "C:\Program Files\Git\bin\git.exe" add .
-& "C:\Program Files\Git\bin\git.exe" commit -m "Update Gift Finder with Amazon affiliate links"
-& "C:\Program Files\Git\bin\git.exe" push origin master
